92ee932e11ec91424800e15ed676575a2e61646a
yqhcode <785458980@qq.com>
2023-01-04 16:59:08 +0800

    feat:add paas base

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/asim/go-micro/plugins/registry/consul/v3"
+	"github.com/asim/go-micro/v3"
+	"github.com/asim/go-micro/v3/registry"
+	common "github.com/yqhcode/paas-common"
+	"log"
+)
+
+func main() {
+	//1.注册中心
+	consulRegistry := consul.NewRegistry(func(option *registry.Options) {
+		option.Addrs = []string{
+			"192.168.65.145:8500",
+		}
+	})
+	//2.配置中心，存放经常变动的变量
+	common.GetConsulConfig
+	common.GetConsulConfig("192.168.65.145", 8500, "/micro/config")
+
+	// 创建服务
+	service := micro.NewService(
+		micro.Name("base"),
+		micro.Version("v0.01"),
+		//添加注册中心
+		micro.Registry(consulRegistry),
+	)
+	// 初始化服务
+	service.Init()
+	// 启动服务
+	if err := service.Run(); err != nil {
+		//输出启动失败信息
+		log.Fatal(err)
+	}
+}
